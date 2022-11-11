0663f5d198e770444219f252f0853b996813c2b0
Kelsey Cahill <kcahill@southleft.com>
2022-11-10 20:53:34 -0500

    Fix for php 8.

--- wp-content/themes/angry-duck/vendor/composer/autoload_static.php
+++ wp-content/themes/angry-duck/vendor/composer/autoload_static.php
@@ -4,13 +4,14 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
+class ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -22,6 +23,7 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
         'S' => 
         array (
             'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Php72\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
             'Symfony\\Contracts\\Translation\\' => 30,
@@ -78,6 +80,10 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
         ),
+        'Symfony\\Polyfill\\Php72\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php72',
+        ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
@@ -112,7 +118,7 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
         ),
         'Psr\\Log\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+            0 => __DIR__ . '/..' . '/psr/log/src',
         ),
         'Psr\\Container\\' => 
         array (
@@ -169,7 +175,7 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
     );
 
     public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/asm89/twig-cache-extension/lib',
+        0 => __DIR__ . '/..' . '/twig/cache-extension/lib',
     );
 
     public static $prefixesPsr0 = array (
@@ -199,6 +205,8 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
     public static $classMap = array (
         'AltoRouter' => __DIR__ . '/..' . '/altorouter/altorouter/AltoRouter.php',
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
         'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
         'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
         'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
@@ -207,11 +215,11 @@ class ComposerStaticInit94b640ad1c1a1de561de8404704f8003
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit94b640ad1c1a1de561de8404704f8003::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit94b640ad1c1a1de561de8404704f8003::$prefixDirsPsr4;
-            $loader->fallbackDirsPsr4 = ComposerStaticInit94b640ad1c1a1de561de8404704f8003::$fallbackDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit94b640ad1c1a1de561de8404704f8003::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit94b640ad1c1a1de561de8404704f8003::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8::$fallbackDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit3904e2c116944a5d9a1a14f68606b3a8::$classMap;
 
         }, null, ClassLoader::class);
     }
